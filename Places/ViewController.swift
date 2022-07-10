cf48871fc958f91d8b97a0b870d86fde9a1c963a
Nusratxoja21 <ganixojayev@internet.ru>
2022-07-10 09:50:39 +0500

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Nusrat Ganiyev on 10/07/22.
+//  Copyright © 2022 Nusrat Ganiyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
