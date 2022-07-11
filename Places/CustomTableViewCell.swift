20c3697e083efe3c4f35d7dec33b84b84c50c60c
Nusratxoja21 <ganixojayev@internet.ru>
2022-07-11 09:14:23 +0500

    Custom Table View Cell

---
+++ Places/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Places
+//
+//  Created by Nusrat Ganiyev on 11/07/22.
+//  Copyright © 2022 Nusrat Ganiyev. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+
+
+}
